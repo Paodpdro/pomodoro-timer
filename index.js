c50d7a962f2e57ab8a22bdcf93566a16a78a93e4
Paodpdro <126562990+Paodpdro@users.noreply.github.com>
2026-07-28 00:40:13 +0300

    First draft of start button is done; timer reducing is working. Pause button is not working properly.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 console.log("hi")
+
+function displayTime() {
+    console.log('display time function working')
+    seconds = '00';
+    minutes = 1;
+    time.textContent = `${minutes}:${seconds}`
+}
+
+function reduceTime() {
+    console.log("Start time working");
+    if (minutes === 0 && seconds === 0) {
+        return
+    }
+
+    if (seconds =='00') {
+        seconds = 59;
+        minutes -=1;
+    }
+    else {
+        seconds -=1
+    }
+
+    if(seconds < 10) {
+        time.textContent = `${minutes}:0${seconds}`;
+    }
+    else {
+        time.textContent = `${minutes}:${seconds}`;
+    }
+
+    
+}
+let seconds;
+let minutes;
+const time = document.querySelector("#time-value");
+const start = document.querySelector("#start-button")
+const pause = document.querySelector("#pause-button");
+
+displayTime();
+start.addEventListener("click", () => {
+    setInterval(reduceTime, 1000);
+});
+
+pause.addEventListener("click", () => {
+    console.log("pause working")
+    clearInterval(reduceTime);
+});
