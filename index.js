b00796d7ef2cf919112e95a2da67ec0d14ab5e01
Paodpdro <126562990+Paodpdro@users.noreply.github.com>
2026-08-03 23:48:05 +0300

    Fix start and pause button

--- index.js
+++ index.js
@@ -8,44 +8,51 @@ function displayTime() {
 }
 
 function reduceTime() {
-    console.log("Start time working");
-    if (minutes === 0 && seconds === 0) {
-        return
-    }
+    if(!isPaused){
+        console.log("Start time working");
+        if (minutes === 0 && seconds === 0) {
+            return
+        }
 
-    if (seconds =='00') {
-        seconds = 59;
-        minutes -=1;
-    }
-    else {
-        seconds -=1
-    }
+        if (seconds =='00') {
+            seconds = 59;
+            minutes -=1;
+        }
+        else {
+            seconds -=1
+        }
 
-    if(seconds < 10) {
-        time.textContent = `${minutes}:0${seconds}`;
-    }
-    else {
-        time.textContent = `${minutes}:${seconds}`;
-    }
+        if(seconds < 10) {
+            time.textContent = `${minutes}:0${seconds}`;
+        }
+        else {
+            time.textContent = `${minutes}:${seconds}`;
+        }
 
+    }
     
 }
 let seconds;
 let minutes;
 let isPaused = false;
+let timerInterval = null;
 const time = document.querySelector("#time-value");
 const start = document.querySelector("#start-button")
 const pause = document.querySelector("#pause-button");
 
 displayTime();
 start.addEventListener("click", () => {
-    if(!isPaused){
-    setInterval(reduceTime, 1000);
+    if(timerInterval !== null) {
+        return
     }
+    isPaused=false;
+    console.log("reduceTime working")
+    timerInterval = setInterval(reduceTime, 1000);
 });
 
 pause.addEventListener("click", () => {
     console.log("pause working")
     isPaused = true;
-    console.log(clearInterval(reduceTime));
+    clearInterval(timerInterval);
+    timerInterval = null
 });
